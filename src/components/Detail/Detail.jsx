e1c881d2d73667fe58950d949e40f3a43972a8ff
harlock184 <105575194+harlock184@users.noreply.github.com>
2023-05-04 11:33:44 -0600

    siguiente lecture

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,15 +1,37 @@
- import React from 'react';
+ import React, { useState, useEffect } from 'react';
  import axios from "axios";
- import {userParams,useNavigate} from "react-router-dom"
+ import {useParams,useNavigate} from "react-router-dom"
 
 
 const Detail = ({onSearch}) => {
+    const {id} = useParams()
+    useEffect(() => {
+        axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+           if (data.name) {
+              setCharacter(data);
+           } else {
+              window.alert('No hay personajes con ese ID');
+           }
+        });
+        return setCharacter({});
+     }, [id]);
+    const [character, setCharacter] = useState({})
 return (
-    <div>
-        Detail
 
+    <div>
+        {
+            character && <div>
+                <h2>{character.name}</h2>
+                <h2>{character.status}</h2>
+                <h2>{character.gender}</h2>
+                <h2>{character.name}</h2>
+
+
+            </div>
+        }
     </div>
-  );
+)
+    
 };
 
 export default Detail;
